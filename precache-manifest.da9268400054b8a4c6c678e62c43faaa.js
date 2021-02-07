0813821e5c6e63a1bc5313950c8c6478703e139e
nhuanthi <tlam1288@gmail.com>
2021-02-07 11:09:28 -0800

    Updates

---
+++ precache-manifest.da9268400054b8a4c6c678e62c43faaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c394dca80b06620f164e33ac1caf13ea",
+    "url": "/Wish-list/index.html"
+  },
+  {
+    "revision": "65b7e94d2946ab7eb241",
+    "url": "/Wish-list/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "4c61c5d4d22827447294",
+    "url": "/Wish-list/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "65b7e94d2946ab7eb241",
+    "url": "/Wish-list/static/js/2.7d43b46f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Wish-list/static/js/2.7d43b46f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c61c5d4d22827447294",
+    "url": "/Wish-list/static/js/main.f385fdc0.chunk.js"
+  },
+  {
+    "revision": "1bb7e2f7afaf87ff782f",
+    "url": "/Wish-list/static/js/runtime-main.93aa898c.js"
+  }
+]);
